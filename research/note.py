cdf42f10452c00412a980b91ff7029c2c298f887
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-04-06 23:55:25 +0530

    player movement tested

--- research/note.py
+++ research/note.py
@@ -16,17 +16,25 @@ image3 = pygame.image.load("assets\\player_right_2.png")
 image4 = pygame.image.load("assets\\player_idle_down.png")
 image5 = pygame.image.load("assets\\player_down_1.png")
 image6 = pygame.image.load("assets\\player_down_2.png")
+image7 = pygame.image.load("assets\\player_idle_left.png")
+image8 = pygame.image.load("assets\\player_left_1.png")
+image9 = pygame.image.load("assets\\player_left_2.png")
+image10 = pygame.image.load("assets\\player_idle_up.png")
+image11 = pygame.image.load("assets\\player_up_1.png")
+image12 = pygame.image.load("assets\\player_up_2.png")
 
 # Create a list of images
 images_right = [image1, image2, image3]
 images_down = [image4, image5, image6]
+images_left = [image7, image8, image9]
+images_up = [image10, image11, image12]
 
-# Set initial image index for running right and running down
-current_image_index_right = 0
-current_image_index_down = 0
+# Set initial image index and direction
+current_image_index = 0
+current_direction = "right"
 
-# Set initial position for the images
-image_x, image_y = 0, SCREEN_HEIGHT // 2
+# Set initial position for the player
+player_x, player_y = 0, SCREEN_HEIGHT // 2
 
 # Set animation speed
 animation_speed = 5
@@ -40,20 +48,34 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                # Move to the next image in the list for running right
-                current_image_index_right = (current_image_index_right + 1) % len(images_right)
+                current_direction = "right"
+                current_image_index = (current_image_index + 1) % len(images_right)
+                player_x += 6
+            elif event.key == pygame.K_LEFT:
+                current_direction = "left"
+                current_image_index = (current_image_index + 1) % len(images_left)
+                player_x -= 6
             elif event.key == pygame.K_DOWN:
-                # Move to the next image in the list for running down
-                current_image_index_down = (current_image_index_down + 1) % len(images_down)
+                current_direction = "down"
+                current_image_index = (current_image_index + 1) % len(images_down)
+                player_y += 6
+            elif event.key == pygame.K_UP:
+                current_direction = "up"
+                current_image_index = (current_image_index + 1) % len(images_up)
+                player_y -= 6
 
     # Clear the screen
     screen.fill((255, 255, 255))
 
-    # Draw the current image for running right
-    screen.blit(images_right[current_image_index_right], (image_x, image_y))
-
-    # Draw the current image for running down
-    screen.blit(images_down[current_image_index_down], (image_x + 100, image_y + 100))
+    # Draw the current image for the player
+    if current_direction == "right":
+        screen.blit(images_right[current_image_index], (player_x, player_y))
+    elif current_direction == "left":
+        screen.blit(images_left[current_image_index], (player_x, player_y))
+    elif current_direction == "down":
+        screen.blit(images_down[current_image_index], (player_x, player_y))
+    elif current_direction == "up":
+        screen.blit(images_up[current_image_index], (player_x, player_y))
 
     # Update the display
     pygame.display.flip()

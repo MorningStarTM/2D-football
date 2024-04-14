acf865633888aa38f9890b910a69e1ec1b40cf57
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-04-14 18:54:12 +0530

    functions test in main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import math
 import random
 from utils.ground import FootballGround
+from utils.keeper import GoalKeeper
 import sys
 
 # Set up the screen dimensions
@@ -12,11 +13,10 @@ def main():
     # Initialize Pygame
     pygame.init()
 
-
     ground = FootballGround(SCREEN_WIDTH, SCREEN_HEIGHT)
-    ground.draw()
+    keeper = GoalKeeper()
 
-    pygame.display.flip()
+    screen = ground.screen
 
     running = True
     while running:
@@ -24,9 +24,41 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+        keys = pygame.key.get_pressed()
+
+        # Check if the right arrow key is held down
+        if keys[pygame.K_RIGHT]:
+            current_direction = "right"
+            keeper.update_position(current_direction)
+            keeper.update_image_index()
+        if keys[pygame.K_LEFT]:
+            current_direction = "left"
+            keeper.update_position(current_direction)
+            keeper.update_image_index()
+        if keys[pygame.K_DOWN]:
+            current_direction = "down"
+            keeper.update_position(current_direction)
+            keeper.update_image_index()
+        if keys[pygame.K_UP]:
+            current_direction = "up"
+            keeper.update_position(current_direction)
+            keeper.update_image_index()
+        # Clear the screen
+        screen.fill((0, 0, 0))
+
+        # Draw the football ground
+        ground.draw()
+        keeper.draw(screen)
+                
+                #keeper.update_image_index()
+                #keeper.update_position(keeper.current_direction)
+        
+
+        # Update the display
+        pygame.display.flip()
+
     pygame.quit()
     sys.exit()
-
 
 if __name__ == "__main__":
     main()
